2795fa09b2caa4ec14eca008d2ca41ef0d0093ae
rosario <rhuancagonza@gmail.com>
2017-03-22 12:07:37 -0500

    Pagina Web de Nutricion

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/nosotros.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "nosotros.html" );
+})
+
+app.get('/clientes.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "clientes.html" );
+})
+
+app.get('/contactanos.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "contactanos.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
